76aa809d789bf02410d163a5e5c86ffa2dd8bf2b
AysuGafarli <133383250+aysugafarli23@users.noreply.github.com>
2024-04-05 14:34:00 +0400

    Update task

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 let span = document.getElementById('span')
-let i = 0
+let i = 1
 let direction = true
 
 let btn = document.getElementById('btn').addEventListener('click', () => {
-  setInterval(() => {
-    if (i >= 10) {
-      direction = false
-    }
-    if (i === 0) {
-      direction = true
-    }
-    if (direction) {
-      span.textContent = count++
-    } else {
-      span.textContent = count--
-    }
-  })
+  if (i >= 10) {
+    direction = false
+  }
+  if (i === 0) {
+    direction = true
+  }
+  if (direction) {
+    span.textContent = i++
+  } else {
+    span.textContent = i--
+  }
 })
